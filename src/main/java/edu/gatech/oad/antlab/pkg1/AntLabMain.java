8b7d176da5422fca439c0e4d6ea6cfdf0de2ec6d
dannyht <dannyhtaylor@gmail.com>
2018-09-04 12:18:59 -0400

    Fixed merge conflict with AntLabMain.java

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -67,18 +67,16 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("Walker Smith");
 		  toPrint += p3.toString("wsmith330");
-                  //Person4 replace P4 with your name
-                  //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("fidel");
-                  toPrint += p4.toString("fefc3");
-                  //Person5 replace P4 with your name
-                  //Person5 replace P5 with your name
-                  //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("Joseph Miano");
-                  toPrint += p5.toString("jmiano3");
+      //Person4 replace P4 with your name
+      //and gburdell4 with your gt id
+      Person4 p4 = new Person4("fidel");
+      toPrint += p4.toString("fefc3");
+      //Person5 replace P4 with your name
+      //Person5 replace P5 with your name
+      //and gburdell5 with your gt id
+      Person5 p5 = new Person5("Joseph Miano");
+      toPrint += p5.toString("jmiano3");
 
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
         System.out.println(toPrint);
 
 
