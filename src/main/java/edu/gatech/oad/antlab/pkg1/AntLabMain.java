d1decbbb3f61f5deaf25d2e5b7afa3f4f3e9e289
jmiano <42718689+jmiano@users.noreply.github.com>
2018-09-06 14:24:02 -0400

    Added @param to main method

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -84,7 +84,8 @@ import edu.gatech.oad.antlab.pkg2.*;
 
     /**
      * entry point for the program
-     */
+     * @param args an array of strings
+     * /
      public static void main(String[] args) {
 
         new AntLabMain().printOutMessage();
